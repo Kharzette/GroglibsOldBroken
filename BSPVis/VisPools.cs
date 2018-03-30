5c1fd993e670cde0ae73d1d8e2f12893b5b98967
Kharzette <6589537+Kharzette@users.noreply.github.com>
2018-03-30 11:43:11 -0500

    Precomputed visibility for bsp maps

---
+++ BSPVis/VisPools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UtilityLib;
+using BSPCore;
+
+
+namespace BSPVis
+{
+	internal class VisPools
+	{
+		public Pool<GBSPPoly>	mPolys;
+		public Pool<VISPStack>	mStacks;
+		public ClipPools		mClipPools;
+		public VISLeaf			[]mVisLeafs;
+		public int				mIterations;
+
+
+		internal VisPools(VISLeaf []leafs, ClipPools cp)
+		{
+			mPolys		=new Pool<GBSPPoly>(() => new GBSPPoly(0));
+			mStacks		=new Pool<VISPStack>(() => new VISPStack());
+			mVisLeafs	=leafs;
+			mClipPools	=cp;
+		}
+	}
+}
