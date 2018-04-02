1e3d9a423d877bc51117161108ca03e8df0cd7f3
Kharzette <6589537+Kharzette@users.noreply.github.com>
2018-04-02 10:49:25 -0500

    Simple CPU driven particles

---
+++ libs/ParticleLib/Particle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpDX;
+using UtilityLib;
+
+
+namespace ParticleLib
+{
+	internal struct Particle
+	{
+		internal Vector3	mPosition;
+		internal float		mSize;
+		internal float		mRotation;
+		internal float		mLifeRemaining;
+		internal Vector4	mColor;
+
+		internal Vector3	mVelocity;
+		internal float		mRotationalVelocity;
+		internal float		mSizeVelocity;
+		internal Vector4	mColorVelocity;		//transparency in W
+
+
+		//return true if expired
+		internal bool Update(float msDelta,
+			Vector3 gravLoc, float gravStr,
+			float maxVelocity)
+		{
+			mLifeRemaining	-=msDelta;
+			if(mLifeRemaining < 0)
+			{
+				return	true;
+			}
+
+			mPosition	+=(mVelocity * msDelta);
+			mColor		+=(mColorVelocity * msDelta);
+			mSize		+=(mSizeVelocity * msDelta);
+			mRotation	+=(mRotationalVelocity * msDelta);
+
+			Vector3	gravVec	=gravLoc - mPosition;
+
+			float	gravDist	=gravVec.Length();
+
+			//normalize
+			gravVec	/=gravDist;
+
+			if(gravDist < 1f)
+			{
+				gravDist	=1f;
+			}
+			gravVec	*=(gravStr / (gravDist * gravDist));
+
+			mVelocity	+=(gravVec * msDelta) / 1000f;
+
+			//clamp velocity
+			float	velLen	=mVelocity.Length();
+
+			if(velLen > maxVelocity)
+			{
+				mVelocity	*=(maxVelocity / velLen);
+			}
+
+			mSize	=MathUtil.Clamp(mSize, 0f, 10000f);
+			mColor	=Mathery.ClampVector(mColor, Vector4.Zero, Vector4.One);
+
+			return	false;
+		}
+	}
+}
---
+++ libs/ParticleLib/ParticleViewDynVB.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.Collections.Generic;
+using MaterialLib;
+using SharpDX;
+using SharpDX.Direct3D11;
+using UtilityLib;
+
+using Buffer	=SharpDX.Direct3D11.Buffer;
+using Device	=SharpDX.Direct3D11.Device;
+using MatLib	=MaterialLib.MaterialLib;
+
+
+namespace ParticleLib
+{
+	internal struct ParticleVert
+	{
+		internal Vector4	mPositionTex;
+		internal Half4		mTexSizeRotBlank;
+		internal Half4		mColor;
+	}
+
+
+	internal class ParticleViewDynVB
+	{
+		Device				mGD;
+		Buffer				mVB;
+		VertexBufferBinding	mVBB;
+
+		ParticleVert		[]mPartBuf;
+
+		MatLib	mMatLib;
+		string	mTexName;
+
+		int		mNumParticles;
+		int		mMaxParticles;
+
+
+		internal ParticleViewDynVB(Device gd, MatLib mats, string texName, int maxParticles)
+		{
+			mGD				=gd;
+			mMatLib			=mats;
+			mMaxParticles	=maxParticles;
+			mTexName		=texName;
+
+			mPartBuf	=new ParticleVert[maxParticles * 6];
+
+			BufferDescription	bDesc	=new BufferDescription(
+				maxParticles * 6 * 32,
+				ResourceUsage.Dynamic, BindFlags.VertexBuffer,
+				CpuAccessFlags.Write, ResourceOptionFlags.None, 0);
+
+			mVB		=Buffer.Create<ParticleVert>(gd, mPartBuf, bDesc);
+			mVBB	=new VertexBufferBinding(mVB, 32, 0);
+		}
+
+
+		internal void FreeAll()
+		{
+			mVB.Dispose();
+		}
+
+
+		internal void Update(DeviceContext dc, Particle []parts, int count)
+		{
+			mNumParticles	=count;
+
+			if(count == 0)
+			{
+				return;
+			}
+
+			Debug.Assert(count <= mMaxParticles);
+
+			for(int i=0;i < count;i++)
+			{
+				Vector4	pos	=Vector4.Zero;
+
+				pos.X	=parts[i].mPosition.X;
+				pos.Y	=parts[i].mPosition.Y;
+				pos.Z	=parts[i].mPosition.Z;
+
+				mPartBuf[i * 6].mPositionTex		=pos;
+				mPartBuf[i * 6 + 1].mPositionTex	=pos;
+				mPartBuf[i * 6 + 2].mPositionTex	=pos;
+				mPartBuf[i * 6 + 3].mPositionTex	=pos;
+				mPartBuf[i * 6 + 4].mPositionTex	=pos;
+				mPartBuf[i * 6 + 5].mPositionTex	=pos;
+
+				//texcoordx
+				mPartBuf[i * 6 + 2].mPositionTex.W	=1;
+				mPartBuf[i * 6 + 4].mPositionTex.W	=1;
+				mPartBuf[i * 6 + 5].mPositionTex.W	=1;
+
+				//texcoordy
+				mPartBuf[i * 6 + 1].mTexSizeRotBlank.X	=1;
+				mPartBuf[i * 6 + 2].mTexSizeRotBlank.X	=1;
+				mPartBuf[i * 6 + 4].mTexSizeRotBlank.X	=1;
+
+				mPartBuf[i * 6].mTexSizeRotBlank.Y		=parts[i].mSize;
+				mPartBuf[i * 6 + 1].mTexSizeRotBlank.Y	=parts[i].mSize;
+				mPartBuf[i * 6 + 2].mTexSizeRotBlank.Y	=parts[i].mSize;
+				mPartBuf[i * 6 + 3].mTexSizeRotBlank.Y	=parts[i].mSize;
+				mPartBuf[i * 6 + 4].mTexSizeRotBlank.Y	=parts[i].mSize;
+				mPartBuf[i * 6 + 5].mTexSizeRotBlank.Y	=parts[i].mSize;
+
+				mPartBuf[i * 6].mTexSizeRotBlank.Z		=parts[i].mRotation;
+				mPartBuf[i * 6 + 1].mTexSizeRotBlank.Z	=parts[i].mRotation;
+				mPartBuf[i * 6 + 2].mTexSizeRotBlank.Z	=parts[i].mRotation;
+				mPartBuf[i * 6 + 3].mTexSizeRotBlank.Z	=parts[i].mRotation;
+				mPartBuf[i * 6 + 4].mTexSizeRotBlank.Z	=parts[i].mRotation;
+				mPartBuf[i * 6 + 5].mTexSizeRotBlank.Z	=parts[i].mRotation;
+
+				mPartBuf[i * 6].mColor		=parts[i].mColor;
+				mPartBuf[i * 6 + 1].mColor	=parts[i].mColor;
+				mPartBuf[i * 6 + 2].mColor	=parts[i].mColor;
+				mPartBuf[i * 6 + 3].mColor	=parts[i].mColor;
+				mPartBuf[i * 6 + 4].mColor	=parts[i].mColor;
+				mPartBuf[i * 6 + 5].mColor	=parts[i].mColor;
+			}
+
+			DataStream	ds;
+			dc.MapSubresource(mVB, MapMode.WriteDiscard, MapFlags.None, out ds);
+
+			for(int i=0;i < (count * 6);i++)
+			{
+				ds.Write<ParticleVert>(mPartBuf[i]);
+			}
+
+			dc.UnmapSubresource(mVB, 0);
+		}
+
+
+		internal void Draw(MatLib mlib, AlphaPool ap,
+			Vector3 pos, Matrix view, Matrix proj)
+		{
+			ap.StoreParticleDraw(mlib, pos, mVBB, mNumParticles * 6, mTexName, view, proj);
+		}
+
+
+		internal void Draw(DeviceContext dc, Matrix view, Matrix proj)
+		{
+			if(mNumParticles <= 0)
+			{
+				return;
+			}
+
+			dc.InputAssembler.SetVertexBuffers(0, mVBB);
+
+			mMatLib.SetMaterialParameter("Particle", "mView", view);
+			mMatLib.SetMaterialParameter("Particle", "mProjection", proj);
+			mMatLib.SetMaterialTexture("Particle", "mTexture", mTexName);
+
+			mMatLib.ApplyMaterialPass("Particle", dc, 0);
+
+			dc.Draw(mNumParticles * 6, 0);
+		}
+
+
+		//write into the depth/normal/material buffer
+		internal void DrawDMN(DeviceContext dc,
+			Matrix view, Matrix proj, Vector3 eyePos)
+		{
+			if(mNumParticles <= 0)
+			{
+				return;
+			}
+
+			dc.InputAssembler.SetVertexBuffers(0, mVBB);
+
+			mMatLib.SetMaterialParameter("ParticleDMN", "mView", view);
+			mMatLib.SetMaterialParameter("ParticleDMN", "mProjection", proj);
+			mMatLib.SetMaterialTexture("ParticleDMN", "mTexture", mTexName);
+
+			mMatLib.ApplyMaterialPass("ParticleDMN", dc, 0);
+
+			dc.Draw(mNumParticles * 6, 0);
+		}
+
+
+		//adds on to the end
+		internal string GetEntityFields(string ent)
+		{
+			ParticleBoss.AddField(ref ent, "tex_name", mTexName);
+
+			return	ent;
+		}
+
+
+		internal void SetTexture(string tex)
+		{
+			mTexName	=tex;
+		}
+
+
+		internal string GetTexture()
+		{
+			return	mTexName;
+		}
+	}
+}
